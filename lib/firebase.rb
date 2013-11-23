dcd121772c588320e503ffcccfb1c52272cd1ecb
Oscar Del Ben <oscar@oscardelben.com>
2013-11-23 12:41:42 +0000

    use instances of Firebase every time

--- lib/firebase.rb
+++ lib/firebase.rb
@@ -1,13 +1,8 @@
 class Firebase
 
-  autoload :Request, 'firebase/request'
+  require 'firebase/request'
 
   class << self
-    attr_accessor :base_uri, :auth
-
-    def base_uri=(other)
-      @base_uri = format_uri(other)
-    end
 
     def format_uri(other)
       if other
@@ -15,88 +10,84 @@ class Firebase
       end
     end
 
-    def auth=(auth)
-      @auth = auth
+    def base_uri=(other)
+      deprecate
+      default_instance.request.uri = format_uri(other)
     end
+
+    def auth=(auth)
+      deprecate
+      default_instance.request.auth = auth
+    end
+
+    def set(path, data)
+      deprecate
+      default_instance.put(path, data)
+    end
+
+    def get(path)
+      deprecate
+      default_instance.get(path)
+    end
+
+    def push(path, data)
+      deprecate
+      default_instance.post(path, data)
+    end
+
+    def delete(path)
+      deprecate
+      default_instance.delete(path)
+    end
+
+    def update(path, data)
+      deprecate
+      default_instance.patch(path, data)
+    end
+
+    def default_instance
+      @default_instance ||= Firebase.new
+    end
+
+    def deprecate
+      puts "[FIREBASE] This syntax has been deprecated. Please upgrade to the new Firebase.new(...).method syntax"
+    end
+
+  end
+
+  attr_accessor :request
+
+  def initialize(base_uri, auth=nil)
+    uri = Firebase.format_uri(base_uri)
+    @request = Firebase::Request.new(uri, auth)
+  end
 
     # Writes and returns the data
-    #   Firebase.set('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
-    def set(path, data)
-      Firebase::Request.put(path, data)
-    end
-
-    # Returns the data at path
-    def get(path)
-      Firebase::Request.get(path)
-    end
-
-    # Writes the data, returns the key name of the data added
-    #   Firebase.push('users', { 'age' => 18}) => {"name":"-INOQPH-aV_psbk3ZXEX"}
-    def push(path, data)
-      Firebase::Request.post(path, data)
-    end
-
-    # Deletes the data at path and returs true
-    def delete(path)
-      Firebase::Request.delete(path)
-    end
-
-    # Write the data at path but does not delete ommited children. Returns the data
-    #   Firebase.update('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
-    def update(path, data)
-      Firebase::Request.patch(path, data)
-    end
-
-  end
-
-  attr_accessor :auth
-  attr_reader :base_uri
-
-  def initialize(base_uri = Firebase.base_uri, auth = Firebase.auth)
-    @base_uri = base_uri
-    @auth = auth
-  end
-
-  def base_uri=(str)
-    @base_uri = Firebase.format_uri(str)
-  end
-
-  def set_base_uri
-    Firebase::Request.set_uri(@base_uri)
-  end
-
-  def set_auth
-    Firebase::Request.set_auth(@auth)
-  end
-
-  def set_request
-    set_base_uri
-    set_auth
-  end
-
+    #   Firebase.eet('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
   def set(path, data)
-    set_request
-    Firebase.set(path, data)
+    request.put(path, data)
   end
 
+  # Returns the data at path
   def get(path)
-    set_request
-    Firebase.get(path)
+    request.get(path)
   end
 
+  # Writes the data, returns the key name of the data added
+  #   Firebase.push('users', { 'age' => 18}) => {"name":"-INOQPH-aV_psbk3ZXEX"}
   def push(path, data)
-    set_request
-    Firebase.push(path, data)
+    request.push(path, data)
   end
 
+  # Deletes the data at path and returs true
   def delete(path)
-    set_request
-    Firebase.delete(path)
+    request.delete(path)
   end
 
+  # Write the data at path but does not delete ommited children. Returns the data
+  #   Firebase.update('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
   def update(path, data)
-    set_request
-    Firebase.update(path, data)
+    request.update(path, data)
   end
 
 end
--- lib/firebase/request.rb
+++ lib/firebase/request.rb
@@ -6,95 +6,82 @@ require 'uri'
 class Firebase
   class Request
 
-    class << self
+    attr_accessor :uri, :auth
 
-      def set_uri(uri)
-        @uri = uri
-      end
-
-      def reset_uri
-        @uri = nil
-      end
-
-      def set_auth(auth)
-        @auth = auth
-      end
-
-      def reset_auth
-        @auth = nil
-      end
-
-      def get(path)
-        process(:get, path)
-      end
-
-      def put(path, value)
-        process(:put, path, :body => value.to_json)
-      end
-
-      def post(path, value)
-        process(:post, path, :body => value.to_json)
-      end
-
-      def delete(path)
-        process(:delete, path)
-      end
-
-      def patch(path, value)
-        process(:patch, path, :body => value.to_json)
-      end
-
-      def build_url(path)
-        host = @uri || Firebase.base_uri
-        path = "#{path}.json"
-        query_string = @auth || Firebase.auth ? "?auth=#{@auth || Firebase.auth}" : ""
-        url = URI.join(host, path, query_string)
-        reset_uri
-        reset_auth
-
-        url.to_s
-      end
-
-      private
-
-      def process(method, path, options={})
-        raise "Please set Firebase.base_uri before making requests" unless Firebase.base_uri || @uri
-
-	      @@hydra ||= Typhoeus::Hydra.new
-        request = Typhoeus::Request.new(build_url(path),
-                                        :body => options[:body],
-                                        :method => method)
-        @@hydra.queue(request)
-        @@hydra.run
-
-        new request.response
-      end
-
+    def initialize(uri, auth=nil)
+      @uri = uri
+      @auth = auth
     end
 
-    attr_accessor :response
-
-    def initialize(response)
-      @response = response
+    def get(path)
+      process(:get, path)
     end
 
-    def body
-      JSON.parse(response.body, :quirks_mode => true)
-    rescue JSON::ParserError => e
-      response.body == 'null' ? nil : raise
+    def put(path, value)
+      process(:put, path, :body => value.to_json)
     end
 
-    def raw_body
-      response.body
+    def post(path, value)
+      process(:post, path, :body => value.to_json)
     end
 
-    def success?
-      [200, 204].include? response.code
+    def delete(path)
+      process(:delete, path)
     end
 
-    def code
-      response.code
+    def patch(path, value)
+      process(:patch, path, :body => value.to_json)
     end
 
+    def build_url(path)
+      host = @uri || Firebase.base_uri
+      path = "#{path}.json"
+      query_string = @auth || Firebase.auth ? "?auth=#{@auth || Firebase.auth}" : ""
+      url = URI.join(host, path, query_string)
+      reset_uri
+      reset_auth
+
+      url.to_s
+    end
+
+    private
+
+    def process(method, path, options={})
+      raise "Please set Firebase.base_uri before making requests" unless Firebase.base_uri || @uri
+
+      @@hydra ||= Typhoeus::Hydra.new
+      request = Typhoeus::Request.new(build_url(path),
+                                      :body => options[:body],
+                                      :method => method)
+      @@hydra.queue(request)
+      @@hydra.run
+
+      new request.response
+    end
+
+  end
+
+  attr_accessor :response
+
+  def initialize(response)
+    @response = response
+  end
+
+  def body
+    JSON.parse(response.body, :quirks_mode => true)
+  rescue JSON::ParserError => e
+    response.body == 'null' ? nil : raise
+  end
+
+  def raw_body
+    response.body
+  end
+
+  def success?
+    [200, 204].include? response.code
+  end
+
+  def code
+    response.code
   end
 end
