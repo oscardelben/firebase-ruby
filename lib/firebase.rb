e465fc71caec32886c8878cfe5ce9e75faf52d50
aharbick@aharbick.com <aharbick@aharbick.com>
2014-02-26 22:15:28 -0500

    Change Firebase to a module and make a Client class.  So that you can use the Firebase provided JWT token generator library in the same app as firebase-ruby

--- lib/firebase.rb
+++ lib/firebase.rb
@@ -1,57 +1,58 @@
-class Firebase
+module Firebase
+  class Client
 
-  require 'firebase/request'
-  require 'firebase/response'
+    require 'firebase/request'
+    require 'firebase/response'
 
-  class << self
-    def format_uri(other)
-      if other
-        other.end_with?("/") ? other : other + '/'
+    class << self
+      def format_uri(other)
+        if other
+          other.end_with?("/") ? other : other + '/'
+        end
       end
     end
-  end
 
-  attr_reader :auth, :request
+    attr_reader :auth, :request
 
-  def initialize(base_uri, auth=nil)
-    uri = Firebase.format_uri(base_uri)
-    @request = Firebase::Request.new(uri)
-    @auth = auth
-  end
+    def initialize(base_uri, auth=nil)
+      uri = Firebase::Client.format_uri(base_uri)
+      @request = Firebase::Request.new(uri)
+      @auth = auth
+    end
 
     # Writes and returns the data
     #   Firebase.set('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
-  def set(path, data, query={})
-    request.put(path, data, query_options(query))
+    def set(path, data, query={})
+      request.put(path, data, query_options(query))
+    end
+
+    # Returns the data at path
+    def get(path, query={})
+      request.get(path, query_options(query))
+    end
+
+    # Writes the data, returns the key name of the data added
+    #   Firebase.push('users', { 'age' => 18}) => {"name":"-INOQPH-aV_psbk3ZXEX"}
+    def push(path, data, query={})
+      request.post(path, data, query_options(query))
+    end
+
+    # Deletes the data at path and returs true
+    def delete(path, query={})
+      request.delete(path, query_options(query))
+    end
+
+    # Write the data at path but does not delete ommited children. Returns the data
+    #   Firebase.update('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
+    def update(path, data, query={})
+      request.patch(path, data, query_options(query))
+    end
+
+    private
+
+    def query_options(query)
+      { :auth => auth }.merge(query)
+    end
   end
-
-  # Returns the data at path
-  def get(path, query={})
-    request.get(path, query_options(query))
-  end
-
-  # Writes the data, returns the key name of the data added
-  #   Firebase.push('users', { 'age' => 18}) => {"name":"-INOQPH-aV_psbk3ZXEX"}
-  def push(path, data, query={})
-    request.post(path, data, query_options(query))
-  end
-
-  # Deletes the data at path and returs true
-  def delete(path, query={})
-    request.delete(path, query_options(query))
-  end
-
-  # Write the data at path but does not delete ommited children. Returns the data
-  #   Firebase.update('users/info', { 'name' => 'Oscar' }) => { 'name' => 'Oscar' }
-  def update(path, data, query={})
-    request.patch(path, data, query_options(query))
-  end
-
-  private
-
-  def query_options(query)
-    { :auth => auth }.merge(query)
-  end
-
 end
 
