3e8fe881d87e16f60e1445d561339101301a1f8b
gobinathal <gobinathal8@gmail.com>
2024-02-17 12:29:27 +0530

    support increment server value

--- lib/firebase/server_value.rb
+++ lib/firebase/server_value.rb
@@ -1,5 +1,9 @@
 module Firebase
   class ServerValue
     TIMESTAMP = { '.sv' => 'timestamp' }.freeze
+
+    def self.increment amount
+      {'.sv' => { 'increment' => amount } }
+    end
   end
 end
